9c997cb04fcc0e2bf0bd95ae588c0a50682cba38
Vitalii Yulieff <wwjurew@gmail.com>
2023-05-02 14:32:40 +0400

    WIP

--- spec/redis_helper.rb
+++ spec/redis_helper.rb
@@ -3,8 +3,7 @@
 def configure_test_redis_db
   conn = AnyCable.broadcast_adapter.redis_conn.connection
   channel = AnyCable.broadcast_adapter.channel
-  new_db_index = conn[:db] + 1
-  new_db_index = 1 if new_db_index > 16
+  new_db_index = conn[:db] + 1 # raises error if > number of redis databases
   url = "redis://#{conn[:host]}:#{conn[:port]}/#{new_db_index}"
 
   AnyCable.broadcast_adapter = :redis, { url: url, channel: channel }
